c221022a900551ef487916c21c4354a6e56443fe
Moshe Pinhasi <moshe.pinhasi@playtech.com>
2016-05-30 23:12:44 +0300

    added - usersPredictsService

--- app/modules/components/usersPredicts/usersPredicts.js
+++ app/modules/components/usersPredicts/usersPredicts.js
@@ -7,74 +7,16 @@ const appModule = angular.module('usersPredicts', [
 	require('./gamesResults/gamesResults')
 ]);
 
-UsersPredictsController.$inject = ['FirebaseService', '$scope'];
-function UsersPredictsController(FirebaseService, $scope) {
+UsersPredictsController.$inject = ['FirebaseService', '$scope', 'usersPredictsService'];
+function UsersPredictsController(FirebaseService, $scope, usersPredictsService) {
 
 	FirebaseService.getUsers().then(results => {
 		const users = _.values(results).map(user => ({id: user.id, value: user.name}));
 		$scope.$apply(()=> this.users = users);
 	});
 
-	//this.users = UsersService.getUsers().map(user => ({id: user.id, value: user.name}));
-
-	this.champion = "England";
-	this.runnerUp = "England";
-
-	this.groups = [
-		[
-			{title: 'Group 1 winner:', value: "England"},
-			{title: 'Group 1 runner up:', value: "England"}
-		],
-		[
-			{title: 'Group 2 winner:', value: "England"},
-			{title: 'Group 2 runner up:', value: "England"}
-		],
-		[
-			{title: 'Group 3 winner:', value: "England"},
-			{title: 'Group 3 runner up:', value: "England"}
-		],
-		[
-			{title: 'Group 4 winner:', value: "England"},
-			{title: 'Group 4 runner up:', value: "England"}
-		]
-	];
-
-	this.onSelectedUser = () => {
-		FirebaseService.getUser(this.userSelected).then(user => {
-			$scope.$apply(()=> {
-				this.userDetails = user;
-				const qulificationPrediction = user.qulificationPrediction;
-				this.champion = qulificationPrediction.champion.firstPlace;
-				this.runnerUp = qulificationPrediction.champion.secondPlace;
-				this.groups = [
-					[
-						{title: 'Group A winner:', value: qulificationPrediction.A.firstPlace},
-						{title: 'Group A runner up:', value: qulificationPrediction.A.secondPlace}
-					],
-					[
-						{title: 'Group B winner:', value: qulificationPrediction.B.firstPlace},
-						{title: 'Group B runner up:', value: qulificationPrediction.B.secondPlace}
-					],
-					[
-						{title: 'Group C winner:', value: qulificationPrediction.C.firstPlace},
-						{title: 'Group C runner up:', value: qulificationPrediction.C.secondPlace}
-					],
-					[
-						{title: 'Group D winner:', value: qulificationPrediction.D.firstPlace},
-						{title: 'Group D runner up:', value: qulificationPrediction.D.secondPlace}
-					],
-					[
-						{title: 'Group E winner:', value: qulificationPrediction.E.firstPlace},
-						{title: 'Group E runner up:', value: qulificationPrediction.E.secondPlace}
-					],
-					[
-						{title: 'Group F winner:', value: qulificationPrediction.F.firstPlace},
-						{title: 'Group F runner up:', value: qulificationPrediction.F.secondPlace}
-					]
-				];
-			});
-		});
-	};
+	this.onSelectedUser = () => FirebaseService.getUser(this.userSelected)
+		.then(user =>$scope.$apply(()=> this.userDetails = usersPredictsService.getUserDetails(user)));
 
 }
 
@@ -86,5 +28,7 @@ appModule.component('usersPredicts', {
 	controller: UsersPredictsController,
 	template: require('./usersPredicts.html')
 });
+
+appModule.factory('usersPredictsService', require('./usersPredictsService'));
 
 module.exports = appModule.name;
