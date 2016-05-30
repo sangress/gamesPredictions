6f98502da269bb89c22f62f993ee3a765ac8f668
Moshe Pinhasi <moshe.pinhasi@playtech.com>
2016-05-30 22:41:48 +0300

    usersPredicts - select users from db + display the groups and champion

--- app/modules/components/usersPredicts/usersPredicts.js
+++ app/modules/components/usersPredicts/usersPredicts.js
@@ -7,8 +7,13 @@ const appModule = angular.module('usersPredicts', [
 	require('./gamesResults/gamesResults')
 ]);
 
-UsersPredictsController.$inject = [];
-function UsersPredictsController() {
+UsersPredictsController.$inject = ['FirebaseService', '$scope'];
+function UsersPredictsController(FirebaseService, $scope) {
+
+	FirebaseService.getUsers().then(results => {
+		const users = _.values(results).map(user => ({id: user.id, value: user.name}));
+		$scope.$apply(()=> this.users = users);
+	});
 
 	//this.users = UsersService.getUsers().map(user => ({id: user.id, value: user.name}));
 
@@ -35,8 +40,40 @@ function UsersPredictsController() {
 	];
 
 	this.onSelectedUser = () => {
-		const user = _.find(this.users,(user) => user.id === this.userSelected);
-		this.userName = user.value;
+		FirebaseService.getUser(this.userSelected).then(user => {
+			$scope.$apply(()=> {
+				this.userDetails = user;
+				const qulificationPrediction = user.qulificationPrediction;
+				this.champion = qulificationPrediction.champion.firstPlace;
+				this.runnerUp = qulificationPrediction.champion.secondPlace;
+				this.groups = [
+					[
+						{title: 'Group A winner:', value: qulificationPrediction.A.firstPlace},
+						{title: 'Group A runner up:', value: qulificationPrediction.A.secondPlace}
+					],
+					[
+						{title: 'Group B winner:', value: qulificationPrediction.B.firstPlace},
+						{title: 'Group B runner up:', value: qulificationPrediction.B.secondPlace}
+					],
+					[
+						{title: 'Group C winner:', value: qulificationPrediction.C.firstPlace},
+						{title: 'Group C runner up:', value: qulificationPrediction.C.secondPlace}
+					],
+					[
+						{title: 'Group D winner:', value: qulificationPrediction.D.firstPlace},
+						{title: 'Group D runner up:', value: qulificationPrediction.D.secondPlace}
+					],
+					[
+						{title: 'Group E winner:', value: qulificationPrediction.E.firstPlace},
+						{title: 'Group E runner up:', value: qulificationPrediction.E.secondPlace}
+					],
+					[
+						{title: 'Group F winner:', value: qulificationPrediction.F.firstPlace},
+						{title: 'Group F runner up:', value: qulificationPrediction.F.secondPlace}
+					]
+				];
+			});
+		});
 	};
 
 }
--- app/modules/views/homepage/homepage.js
+++ app/modules/views/homepage/homepage.js
@@ -8,7 +8,7 @@ HomepageController.$inject = ['userDetails', 'FacebookService', '$scope', '$stat
 function HomepageController(userDetails, FacebookService, $scope, $state) {
 
 	this.user = userDetails;
-	this.selectedPage = "qulificationPrediction";
+	this.selectedPage = "usersPredicts";
 	this.tabClicked = (id) => this.selectedPage = id;
 
 	this.userPredictionsClicked = (id) => this.tabClicked('usersPredicts');
